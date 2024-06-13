a511489a05c9f7e3d81db3454c3caff2a8cd2a66
battistaennio <ennio.battista99@gmail.com>
2024-06-13 15:29:13 +0200

    feat: add js button selector and event on click

--- js/main.js
+++ js/main.js
@@ -2,3 +2,13 @@
 //Ogni cella ha un numero progressivo, da 1 a 100.
 //Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 //Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+//seleziono il bottone gioca
+const playButton = document.querySelector("button");
+
+//creo evento su click
+playButton.addEventListener("click",
+    function (){
+        console.log("funziono");
+    }
+)
